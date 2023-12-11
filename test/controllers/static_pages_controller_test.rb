315c082b0ba74bafedc205a0028acf072086eb77
Khaled Mohammad <41228415+khaledyasser2@users.noreply.github.com>
2023-12-11 17:47:12 +0900

    links all work but do nothing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @base_title="WasedaPlace"
+  end
+  
+  test "all the links work" do
+    get root_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+
+    get about_path
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
