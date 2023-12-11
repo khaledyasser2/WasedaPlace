324dbbc2819e1c06300d5e4e8206246f6e893b51
Khaled Mohammad <41228415+khaledyasser2@users.noreply.github.com>
2023-12-11 17:34:52 +0900

    links exist but none work yet

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,22 @@
 module SessionHelper
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      #@current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user=user
+      end
+
+    elsif cookies.encrypted[:user_id]
+      user = User.find_by(id: cookies.encrypted[:user_id])
+      if user&.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @curent_user=user
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # root "articles#index"
+  root "static_pages#home"
+  get '/help', to:'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get "/login", to: "sessions#new"
+  get '/signup', to: "users#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
