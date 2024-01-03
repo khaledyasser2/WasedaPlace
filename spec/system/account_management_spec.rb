9bd2932e6fc48cb774678d60e3957acf7bc2abde
Khaled Mohammad <khaled.mohammad@techouse.jp>
2024-01-03 19:01:14 +0900

    user creation complete

--- spec/system/account_management_spec.rb
+++ spec/system/account_management_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "AccountManagements", type: :system do
   end
   
   scenario "user registers account" do
+    n_users = User.count
     visit root_path
     click_on "Sign up now!"
 
@@ -15,5 +16,12 @@ RSpec.describe "AccountManagements", type: :system do
     fill_in "Password confirmation", with: "jojomojo"
     
     click_on "Create my account"
+
+    aggregate_failures do
+      expect(User.count).to equal n_users+1
+      expect(User.last[:name]).to eq "jojo"
+      expect(User.last[:password]).to eq nil #since it's obscured
+      expect(User.last[:password_confirmation]).to eq nil
+    end
   end
 end
