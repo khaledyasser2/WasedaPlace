676243dbd02c96e9309db24c2967eccde1ba968b
Khaled Mohammad <khaled.mohammad@techouse.jp>
2024-01-03 18:37:19 +0900

    preliminary user showing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
 
   def create
     p params
-    #@user = User.create(
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get "/login", to: "sessions#new"
   get '/signup', to: "users#new"
-  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
   resources :users
 end
