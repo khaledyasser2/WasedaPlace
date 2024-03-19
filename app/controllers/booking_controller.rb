223a5dbb54987c888d0c674ffcff94c754e76b66
Khaled Mohammad <41228415+khaledyasser2@users.noreply.github.com>
2024-03-20 01:01:49 +0200

    can parse the jsons now. Need data validation and tests to find available slots then display them

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,5 +1,15 @@
 class BookingController < ApplicationController
   
   def index
+    file_path = Rails.root.join('public', 'data', 'ART.json')
+    class_schedules = JSON.parse(File.read(file_path))
+    @available_slots = process_schedules_for_availability(class_schedules)
+  end
+
+private
+
+  def process_schedules_for_availability(schedules)
+    parsed = schedules.map { |s| {name: s["b"], day: s["i"][0]["d"], period: s["i"][0]["p"], room: s["I"][0]["l"] } }
+    all=[]
   end
 end
